97bd07d400f3ce77339b989025b71755e9255212
Ahmed <ahmaat19@gmail.com>
2023-08-14 15:39:21 +0300

    init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className='flex h-[90vh] flex-col items-center justify-center p-24'>
+      <h1>Alhilaal Real Estate</h1>
+    </main>
+  )
+}
