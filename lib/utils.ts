58858e5e68a28ac76e44663f490e1f0a38d10706
abdizamedmo <abdizamedmo@gmail.com>
2023-09-20 11:32:13 +0300

    Utils func added

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,16 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+import { format } from 'date-fns'
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
 }
+
+export const fCurrency = (amount: number) => {
+    return new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+    }).format(amount)
+}
+
+export const fDate = (date: Date) => format(date, 'MM/dd/yyyy')
